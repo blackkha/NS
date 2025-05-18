6adf70a104654d3df6a24aabcfebd2e7432ad768
blackkha <kitsuragi@icloud.com>
2025-05-18 10:33:08 +0300

    1

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NullSigLanding from "./NullSigLanding.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<NullSigLanding />);
